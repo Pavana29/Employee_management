7679cf140190ebe17582b0c2a59ee6a46308432f
Pavana R <145240496+Pavana29@users.noreply.github.com>
2025-12-19 12:33:01 +0530

    intial commit

---
+++ employees/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.employee_list, name='employee_list'),
+    path('add/', views.add_employee, name='add_employee'),
+    path('<int:pk>/', views.employee_detail, name='employee_detail'),
+]
